9045f3a5d826b2b28ed037dc15149153e0e46439
Andrey <skpuralsk@yandex.ru>
2017-11-03 00:33:06 +0300

    add typeorm and first implementation add/get objects

--- src/server-app/server.ts
+++ src/server-app/server.ts
@@ -1,11 +1,14 @@
+import 'reflect-metadata';
 import * as Koa from 'koa';
+import {createConnection} from 'typeorm';
 import { config } from "./src/config/config";
 import { routes } from './src/middleware/routes';
 import { logger } from "./src/middleware/logger";
 
-const app = new Koa();
-app.use(logger);
-app.use(routes);
-app.listen(config.port);
-
-console.log(`server running on port ${config.port}`);
+createConnection().then(async () => {
+    const app = new Koa();
+    app.use(logger);
+    app.use(routes);
+    app.listen(config.port);
+    console.log(`server running on port ${config.port}`);
+}).catch(error => console.log(`TypeORM connection error: ${error}`));
--- src/server-app/src/middleware/routes.ts
+++ src/server-app/src/middleware/routes.ts
@@ -1,4 +1,6 @@
 import * as Router from 'koa-router';
+import {getManager} from "typeorm";
+import {User} from "../db/entity/User";
 
 const router = new Router();
 
@@ -7,8 +9,20 @@ router.get('/', async (ctx) => {
 });
 
 router.get('/test', async (ctx) =>{
+    const userRepository = getManager().getRepository(User);
+    const users = await userRepository.find();
     ctx.status = 200;
-    ctx.body = "TEST";
+    ctx.body = users;
+});
+
+router.get('/lol', async (ctx) =>{
+    const userRepository = getManager().getRepository(User);
+
+    const newUser = userRepository.create({firstName: 'Andrey', lastName: "Karnaukhov", age: 26});
+
+    await userRepository.save(newUser);
+
+    ctx.body = newUser;
 });
 
 export const routes = router.routes();
